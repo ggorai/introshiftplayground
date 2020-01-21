7866cf4d37b9f38f578d4d65a2f8766764e5fab4
CM Student <cmstudent@UHWO-ACM-C-14.local>
2020-01-21 10:35:50 -1000

    complete 1 through 7

--- shiftplayground1.playground/Contents.swift
+++ shiftplayground1.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//var myfirstvariable: String
+
+var myfirstvariable = "I've created my first string"
+
+//Declare and assign variable in single line of code
+var mysecondvariable = "and look I addede two strings together"
+
+var mythirdvariable = myfirstvariable + mysecondvariable
+
+print(mythirdvariable)
