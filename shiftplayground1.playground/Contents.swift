60e2ae859a8ced0431ad02946aaca669f0d67c5a
CM Student <cmstudent@uhwo-acm-c-14.local>
2020-01-23 09:55:53 -1000

    completed part 04

--- shiftplayground1.playground/Contents.swift
+++ shiftplayground1.playground/Contents.swift
@@ -26,4 +26,9 @@ var Garrison3variable = "ordinary day"
 
 var Garrison4variable = GarrisonVariable + Garrison2variable
 
-print (Garrison4variable)
+print(Garrison4variable)
+
+//assignnment #2 True or False
+
+var appState = false
+print(appState)
